d5b9832c7af488cb47bcbd60e2208b4daf6dff72
mr-ceo7 <kassimmusa322@gmail.com>
2026-04-18 16:50:18 +0300

    Replace Chama social preview card

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -49,8 +49,8 @@ export function SEO({
       <meta property="og:image" content={image} />
       <meta property="og:image:secure_url" content={image} />
       <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="1024" />
-      <meta property="og:image:height" content="1024" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
